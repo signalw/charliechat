81318fbb67b0eeb466558cdfb63dc6af5c7d4549
signalw <xinhao@brandeis.edu>
2016-11-12 11:31:56 -0500

    allow all hosts

--- CharlieChat/settings.py
+++ CharlieChat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "placeholder_for_secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
