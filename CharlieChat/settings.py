540bec6feb6065b4b3686efb4e77029c837140d8
signalw <xinhao@brandeis.edu>
2016-11-12 09:07:03 -0500

    set up env var for secret_key

--- CharlieChat/settings.py
+++ CharlieChat/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-from local_settings import secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key
+SECRET_KEY = os.environ.get("SECRET_KEY", "placeholder_for_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
