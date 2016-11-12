bf463be28b211c2e9db14b9f9b3b62b9601359d6
signalw <xinhao@brandeis.edu>
2016-11-12 11:10:30 -0500

    configured static assets settings for production

--- CharlieChat/settings.py
+++ CharlieChat/settings.py
@@ -117,14 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_ROOT = '/var/cache/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
