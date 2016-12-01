88d4ad22b8a511de0fe24d2dff439e90cfa9d376
signalw <xinhao@brandeis.edu>
2016-11-30 20:46:49 -0500

    redirect after post

--- main/views.py
+++ main/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
-#from django.template import loader
-
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 def index(request):
-    request.session['_messages'] = request.session.get('_messages', "Conversations will be displayed below:\n")
+    request.session['_messages'] = request.session.get('_messages',
+        "Conversations will be displayed below:\n")
     if request.method == "GET":
-        return render(request, 'index.html', {'messages': request.session['_messages']})
+        return render(request, 'index.html',
+            {'messages': request.session['_messages']})
     else:
         request.session['_messages'] += request.POST.get('query')+"\n"
-        return render(request, 'index.html', {'messages': request.session['_messages']})
+        return HttpResponseRedirect(reverse('index'))
