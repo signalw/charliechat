508a90acda69f65b46633eeee4ac7e4df0c858a9
Jessica Huynh <jessicahhuynh@outlook.com>
2016-12-04 02:27:47 -0500

    check if config module is present

--- CharlieChat/settings.py
+++ CharlieChat/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import config
+import importlib
+config_present = importlib.util.find_spec("config") # if there's no config, we're on heroku
+if config_present:
+    import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
