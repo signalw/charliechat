b234da18b6492442624fc92d253a045e0e2cd37f
Jessica Huynh <jessicahhuynh@outlook.com>
2016-12-11 11:31:56 -0500

    check for empty route

--- main/utils/queryGoogleForDirections.py
+++ main/utils/queryGoogleForDirections.py
@@ -20,23 +20,26 @@ def getPath(orig, dest):
 
 	response = requests.get(url,params=params)
 	r = response.json() # gets us a dictionary
+	print(r)
+	if len(r['routes']) > 0:
+		txt = r['routes'][0]
+		arrivaltime = txt['legs'][0]['arrival_time']['text']
+		departtime = txt['legs'][0]['departure_time']['text']
+		steps = txt['legs'][0]['steps']
 
-	txt = r['routes'][0]
-	arrivaltime = txt['legs'][0]['arrival_time']['text']
-	departtime = txt['legs'][0]['departure_time']['text']
-	steps = txt['legs'][0]['steps']
-
-	directionsraw = str(r).split('html_instructions')[1:]
-	directions_cleaner = ""
-	for item in directionsraw:
-		path = item[:item.find("'distance'")][4:-3].split("',")[0] + "\n"
-		directions_cleaner = directions_cleaner + path
+		directionsraw = str(r).split('html_instructions')[1:]
+		directions_cleaner = ""
+		for item in directionsraw:
+			path = item[:item.find("'distance'")][4:-3].split("',")[0] + "\n"
+			directions_cleaner = directions_cleaner + path
 
 
-	directions_cleaner = re.sub(r'(\<.*?\>)','',directions_cleaner)
+		directions_cleaner = re.sub(r'(\<.*?\>)','',directions_cleaner)
 
-	print(directions_cleaner)
-	print('\nleave at', departtime)
-	print('\narrive by', arrivaltime)
+		print(directions_cleaner)
+		print('\nleave at', departtime)
+		print('\narrive by', arrivaltime)
 
-	return directions_cleaner + '\nLeave at ' + departtime + '\nArrive by ' + departtime
+		return directions_cleaner + '\nLeave at ' + departtime + '\nArrive by ' + departtime
+	else:
+		return "We can't seem to connect at this time."
