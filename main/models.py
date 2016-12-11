3512e3326b15aae7d38c9d60aebe45be6c5224b5
signalw <xinhao@brandeis.edu>
2016-12-11 16:10:10 -0500

    added query and profile model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+@python_2_unicode_compatible
+class Query(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    raw_query = models.TextField(max_length=500)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.raw_query
+
+@python_2_unicode_compatible
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    identity = models.TextField(max_length=50, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
