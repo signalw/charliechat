bbe071d825e790d5c8bbb426547e869766385747
signalw <xinhao@brandeis.edu>
2016-12-12 14:04:43 -0500

    combined yelp methods in one file

--- main/utils/yelpfusionapi.py
+++ main/utils/yelpfusionapi.py
@@ -33,8 +33,8 @@ except ImportError:
 # OAuth credential placeholders that must be filled in by users.
 # You can find them on
 # https://www.yelp.com/developers/v3/manage_app
-CLIENT_ID = ''
-CLIENT_SECRET = ''
+CLIENT_ID = 'jlFyKyAo15RPiGx3sOJrWQ'
+CLIENT_SECRET = 'z9KsqHuvkadpF6eG8JYZClvaVniZEwzAOlbRHEXXBHht2w74c05xeD2iHlSweDnp'
 
 
 # API constants, you shouldn't have to change these.
@@ -233,6 +233,88 @@ def main():
             )
         )
 
-
 # if __name__ == '__main__':
 #     main()
+# def formatResponses(responseList):
+# 	"""Given a list of JSON responses, return their Python object form
+
+#     Args:
+#        responseList: list of http responses, i.e. given by query_api
+
+#     Returns:
+#     	parsedResponseList: list after JSON decoding
+
+# 	"""
+# 	formattedResponseList = []
+# 	for response in responseList:
+# 		data = json.loads(response)
+# 		formattedResponseList.append(data)
+# 	return formattedResponseList
+
+def parseResponses(termToResponseList):
+	"""
+	Args:
+		dict: {term:responsesForTermList}
+	Returns:
+		dict: {term: businessInfoList}
+				businessInfoList: {businessName: businessInfo}
+
+		e.g. dict[terms][businessNames][businessInfo]
+	"""
+
+	# for response in termToResponseList:
+	# 	print(type(response))
+
+	termToBusinessDict = {}
+	for term in termToResponseList:
+		businessInfoList = {}
+		for response in termToResponseList[term]:
+			infoDict = {}
+			infoDict['name'] = response['name']
+			infoDict['rating'] = response['rating']
+			infoDict['price'] = response['price']
+			infoDict['distance'] = response['distance']
+			infoDict['url'] = response['url']
+			businessInfoList[response['name']] = infoDict
+		termToBusinessDict[term] = businessInfoList
+	return termToBusinessDict
+
+def getUserType(userType):
+	"""
+	Args:
+		userType (str): type of user
+	Returns:
+		list of search terms for the designated user
+	"""
+
+	def student():
+		terms = ['coffee', 'cafe', 'thrift', 'active life',
+				'arts and entertainment']
+		return terms
+	def tourist():
+		terms = ['points of interest', 'museum', 'restaurants', 'shopping',
+				'arts and entertainment', 'local flavor']
+		return terms
+	def worker():
+		terms = ['coffee', 'cafe', 'nightlife']
+		return terms
+
+	options = {
+		'student': student,
+		'tourist': tourist,
+		'worker': worker,
+	}
+	terms = []
+
+
+	return options[userType]()
+
+# searchTermList = getUserType('student')
+# businesses = query_multiterm(["food"], '42.3754247,-71.2220716', '3')
+# parsed = parseResponses(businesses)
+# print([name for term in parsed for name in parsed[term]])
+# for term in parsed:
+# 	print(term)
+# 	for businessName in parsed[term]:
+# 		print(businessName)
+# 	print('\n')
